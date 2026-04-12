c460626d93c3f30c90c5eb11457636e3d62938a4
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-04-13 02:40:50 +0600

    проверяем точку в части после запятой если в input не будет точки или она будет начинатся с точки или заканчиватся ею то там ошибка

--- a.py
+++ a.py
@@ -5,3 +5,6 @@ def mail(email):
     symbol = email.split("@")
     if len(symbol) != 2:
         return False    
+    dot = symbol[1]
+    if "." not in dot or dot.startswith(".") or dot.endswith("."):    
+        return False
