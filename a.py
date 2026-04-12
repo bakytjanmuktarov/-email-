b6e39b865552def50209e782ee45466f3f13ffac
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-04-13 02:05:54 +0600

    добавляем проверку на то что во второй половине где обычно gmail.com точка находится не в конце и не в начале

--- a.py
+++ a.py
@@ -5,3 +5,7 @@ def mail(email):
     a = email.split("@")
     if len(a) != 2:
         return False
+    b = a[1]
+    if "." not in b or b.startswith(".") or b.endswith("."):
+        return False
+    return True
